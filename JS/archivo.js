2404dbb417b74496f3d609b7ba5dae55c3ac8134
bazan ivan <ivanbazan12xd@gmail.com>
2022-09-09 23:57:09 -0300

    cositas de sass y js

---
+++ JS/archivo.js
@@ -0,0 +1,7 @@
+let papafrita = prompt ("¿que Tenes un paquete o una bolsa?");
+
+if (papafrita = "paquete") {
+    alert ("tenes un paquete de papas fritas")
+}
+
+else { alert("no tenes nada mostro") }
