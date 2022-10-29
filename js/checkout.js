d3d882ee0cbaf53e53f18f5727c302b3fecc728f
bazan ivan <ivanbazan12xd@gmail.com>
2022-10-29 13:48:43 -0300

    intente agregar el checkout pero tengo un dilema con el carrito

---
+++ js/checkout.js
@@ -0,0 +1,71 @@
+const carrito = []
+const btnComprar = document.querySelector("#btnComprar")
+
+const recuperarCarrito = ()=> {
+    if (localStorage.getItem("carrito")) {
+        let carritoRecuperado = JSON.parse(localStorage.getItem("carrito")) || []
+            carritoRecuperado.forEach(producto => carrito.push(producto))
+    } else {
+        console.warn("No se encontró un carrito previamente guardado.")
+    }
+}
+
+const cargarCarrito = ()=> {
+    let tablaBody = ""
+    const tbody = document.querySelector("tbody")
+          tbody.innerHTML = ""
+          carrito.forEach(fruta => {
+            tablaBody += `<tr>
+                            <th class="centrar">${fruta.imagen}</th>
+                            <th>${fruta.nombre}</th>
+                            <th>$ ${fruta.precio}</th>
+                            <th><button id="${fruta.nombre}" class="button-delete button-small">x</button></th>
+                        </tr>`
+          })
+          tbody.innerHTML = tablaBody
+    let totalCarrito = carrito.reduce((acc, item)=> acc + item.precio, 0 )
+        tbody.innerHTML += `<tr>
+                                <th></th>
+                                <th>TOTAL</th>
+                                <th>$ ${totalCarrito.toFixed(2)}</th>
+                            </tr>`
+        activoBotonesDelete() //Una vez que armé toda la tabla, activo el click en los botones DELETE 
+}
+
+const activoBotonesDelete = ()=> { //ARMO ARRAY DE BOTONES DELETE
+    const btnsDelete = document.querySelectorAll(".button-delete.button-small")
+          btnsDelete.forEach(btn => {
+            btn.addEventListener("click", (e)=> { //LES ASIGNO EL EVENTO CLICK 
+                //BUSCO INDEX DE UN PRODUCTO EN CARRITO Y LO ELIMINO CON SPLICE() 
+                let aEliminar = carrito.findIndex(fruta => fruta.nombre === e.target.id) 
+                    carrito.splice(aEliminar, 1)
+                    localStorage.setItem("carrito", JSON.stringify(carrito)) //ACTUALIZO CARRITO EN LOCALSTORAGE 
+                    cargarCarrito() //RECARGO CARRITO EN TABLE Y SE ACTUALIZAN LOS PRODUCTOS 
+            })
+          })
+}
+
+//INTEGRO SWEET ALERT DE FORMA GENERICA
+const alerta = (titulo, textoBoton)=> {
+    return Swal.fire({title: titulo, confirmButtonText: textoBoton})
+}
+
+const carritoVacio = ()=> {
+      alerta("¡El carrito está vacío!", "OPS!")
+}
+
+const finalizarCompra = ()=> {
+    alerta('Muchas gracias por su compra.', 'FINALIZAR').then(result => {
+        if (result.isConfirmed) {
+            localStorage.removeItem("carrito")
+            location.href = 'index.html'
+        }
+    })
+}
+
+btnComprar.addEventListener("click", ()=> carrito.length === 0 ? carritoVacio() : finalizarCompra() )
+
+//CARGAR AL INICIO DEL FORMULARIO HTML
+
+recuperarCarrito()
+carrito.length > 0 && cargarCarrito()
--- js/dom.js
+++ js/dom.js
@@ -13,13 +13,52 @@ const cargarMisProductos = ()=> {
     productos.forEach(producto => container.innerHTML += retornoCard(producto))
     activarBotonesAdd() //uso la funcion para activar el evento click en los botones
 }
+
 cargarMisProductos()
 
-const agregarAlCarrito = (e)=> {  //AGREGAR UN PRODUCTO AL CARRITO
+
+//PLANTILLA DE NOTIFICACION CON LIBRERIA TOASTIFY
+
+const toast = (mensaje) => {
+    Toastify({
+        text: mensaje,
+        duration: 3000,
+        close: false,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+      }).showToast();
+}
+
+//AGREGAR UN PRODUCTO AL CARRITO
+
+const agregarAlCarrito = (e)=> {
     let resultado = productos.find(prod => prod.nombre === e.target.id)
         if (resultado !== undefined) {
             carrito.push(resultado)
-            console.clear()
-            console.table(carrito)
-        }
+            guardarCarrito()
+            toast(`'${e.target.id}' es un elemento del carrito ahora.`)
+            }
 }
+
+const guardarCarrito = () => { //ACA USO OPERADOR LOGICO
+    carrito.length > 0 && localStorage.setItem("carrito", JSON.stringify("carrito"))
+}
+
+const recuperarCarrito = ()=> {
+    if (localStorage.getItem("carrito")) {
+        let carritoRecuperado = JSON.parse(localStorage.getItem("carrito")) || []
+            carritoRecuperado.forEach(producto => carrito.push(producto))
+    } else {
+        console.warn("No se encontró un carrito previamente guardado.")
+    }
+}
+
+//si hay un carrito previo 
+recuperarCarrito()
+
+//cargar productos para comprar
+cargarMisProductos()
